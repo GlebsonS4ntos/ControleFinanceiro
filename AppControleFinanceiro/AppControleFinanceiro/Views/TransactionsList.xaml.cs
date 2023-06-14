b81ed93aef07102e073e47ea5748de149079f07b
Glebson Santos <glebsonsantos@outlook.pt>
2023-06-14 02:59:25 -0300

    Deixando a listagem das transacoes dinamica

--- AppControleFinanceiro/AppControleFinanceiro/Views/TransactionsList.xaml.cs
+++ AppControleFinanceiro/AppControleFinanceiro/Views/TransactionsList.xaml.cs
@@ -1,14 +1,19 @@
+using AppControleFinanceiro.Repositories;
+
 namespace AppControleFinanceiro.Views;
 
 public partial class TransactionsList : ContentPage
 {
 	private readonly CreateTransaction _createTransaction;
 	private readonly UpdateTransaction _updateTransaction;
-	public TransactionsList(CreateTransaction createTransaction, UpdateTransaction updateTransaction)
+	private readonly ITransactionRepository _transactionRepository;
+	public TransactionsList(CreateTransaction createTransaction, UpdateTransaction updateTransaction, ITransactionRepository transactionRepository)
 	{
 		_createTransaction = createTransaction;
 		_updateTransaction = updateTransaction;
+		_transactionRepository = transactionRepository;
 		InitializeComponent();
+		ViewListTransaction.ItemsSource = _transactionRepository.GetAll();
 	}
 	private void GoToCreateTransaction(Object sender, EventArgs args)
 	{
