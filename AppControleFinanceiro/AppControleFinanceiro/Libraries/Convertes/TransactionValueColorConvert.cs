8be752be534505a6e60b4e0b56288093e6290784
Glebson Santos <glebsonsantos@outlook.pt>
2023-06-20 08:58:34 -0300

    Customizando a exibicao dos valores das transacoes usando Convert

---
+++ AppControleFinanceiro/AppControleFinanceiro/Libraries/Convertes/TransactionValueColorConvert.cs
@@ -0,0 +1,35 @@
+﻿using AppControleFinanceiro.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppControleFinanceiro.Libraries.Convertes
+{
+    public class TransactionValueColorConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Transaction t = (Transaction)value;
+            if (t == null)
+            {
+                return Colors.Black; 
+            }
+            else if (t.Type == Enum.TransactionType.Income)
+            {
+                return Color.FromArgb("#FF939E5A");
+            }
+            else
+            {
+                return Colors.Red;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AppControleFinanceiro/AppControleFinanceiro/Libraries/Convertes/TransactionValueConverter.cs
@@ -0,0 +1,35 @@
+﻿using AppControleFinanceiro.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppControleFinanceiro.Libraries.Convertes
+{
+    public class TransactionValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Transaction t = (Transaction) value;
+            if(t == null)
+            {
+                return string.Empty;
+            }
+            else if (t.Type == Enum.TransactionType.Income)
+            {
+                return t.Value.ToString("C");
+            }
+            else
+            {
+                return "-" + t.Value.ToString("C");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
